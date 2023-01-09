915db6f7b78e8415845ec7762498a6b9b4dcbc21
maheshjindal555 <maheshjindal555@gmail.com>
2023-01-09 16:15:49 +0530

    Second Commit

---
+++ greater.py
@@ -0,0 +1,8 @@
+import sys
+a = 10
+b = 20
+
+if a > b:
+    print("\n" + f"a is greater with value: {a}")
+else:
+    print("\n" + f"b is greater with value: {b}")
---
+++ sum.py
@@ -0,0 +1,6 @@
+import sys
+a = 10
+b = 20
+
+sum = a + b
+print("\n" + f"Sum of Numbers {a} and {b} is: {sum}")
